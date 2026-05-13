c554702346a577cdc6de680d4c57cfcda9368b57
Maanas Kauntia <maanaskauntia@gmail.com>
2026-05-13 22:20:54 +0530

    Update E-Commerce Seller Quality.sql

--- E-Commerce Seller Quality.sql
+++ E-Commerce Seller Quality.sql
@@ -1,10 +1,10 @@
-WITH Economic_And_Customer_Metrics AS
+WITH Economic_Value_And_Customer_Feedback AS
 (
 	SELECT i.seller_id, 
 			COUNT(DISTINCT i.order_id) AS total_orders,
 			round(SUM(i.price),2) as GMV, 
 			round((round(SUM(i.price),2)/COUNT(DISTINCT i.order_id)),2) as AOV,
-			-- Calculation of Distinct Active days in last 180 days
+			-- Calculation of Distinct Active days in last 180 days (Recent Seller Activity)
 			COUNT(DISTINCT CASE 
 	        WHEN o.order_purchase_timestamp >= '2018-04-20' --last purchase on 17th Oct '18 
 	        THEN DATE(o.order_purchase_timestamp) 
@@ -25,11 +25,11 @@ Economic_And_Customer_Metrics_Percentiles AS
 			round(PERCENT_RANK() OVER (ORDER BY AOV),2) AS AOV_p,
 			round(PERCENT_RANK() OVER (ORDER BY active_days_in_180d),2) AS recent_activity_p,
 			round(PERCENT_RANK() OVER (ORDER BY avg_review_score),2) AS customer_reviews_p
-	FROM Economic_And_Customer_Metrics
+	FROM Economic_Value_And_Customer_Feedback
 ),
 
 
-Operational_Reliability_Percentile AS
+Operational_Rigor_Percentile AS
 (
     SELECT 
         i.seller_id,
@@ -41,7 +41,7 @@ Operational_Reliability_Percentile AS
 								                CAST(NULLIF(o.order_delivered_carrier_date, '') AS TIMESTAMP),
 								                CAST(NULLIF(i.shipping_limit_date, '') AS TIMESTAMP)
 								            ) --negative hrs means delay
-								        )),2) as ops_reliability_p
+								        )),2) as shipping_reliability
     FROM "SQL projects".main.olist_order_items_dataset AS i
     JOIN "SQL projects".main.olist_orders_dataset AS o 
         ON i.order_id = o.order_id
@@ -53,9 +53,9 @@ Sellers_Scored AS
 (
 	SELECT ec.seller_id, ec.total_orders,
 		    -- Weighted Average calculation
-            (ec.GMV_p * 0.30 + ops.ops_reliability_p * 0.25 + ec.customer_reviews_p * 0.20 + ec.recent_activity_p * 0.15 + AOV_p * 0.10) AS final_score
+            (ec.GMV_p * 0.30 + ops.shipping_reliability * 0.25 + ec.customer_reviews_p * 0.20 + ec.recent_activity_p * 0.15 + AOV_p * 0.10) AS final_score
 FROM Economic_And_Customer_Metrics_Percentiles AS ec 
-JOIN Operational_Reliability_Percentile AS ops ON ec.seller_id=ops.seller_id
+JOIN Operational_Rigor_Percentile AS ops ON ec.seller_id=ops.seller_id
 )
 
 SELECT seller_id, total_orders,
@@ -68,4 +68,3 @@ SELECT seller_id, total_orders,
 	    END AS seller_tier
 FROM Sellers_Scored
 ORDER BY final_score DESC
-
